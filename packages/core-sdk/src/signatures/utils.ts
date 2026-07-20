badbb1594345a68a60087349a3602d0aa699a4f8
uzzam <42221362+u-zzam@users.noreply.github.com>
2026-07-20 19:42:59 +0900

    refactor(iris-sdk): remove unused import

--- packages/core-sdk/src/signatures/utils.ts
+++ packages/core-sdk/src/signatures/utils.ts
@@ -1,4 +1,2 @@
-import { bytesToBigInt } from "viem";
-
 /** Given `isNonceUsed` in our contract is mapped by the `authorizer`, we can use simpler form as solvers are never going to response quotes in the same ms  */
 export const randomNonce = () => BigInt(Date.now());
